9a7cd94a87f2654b0e0bb337f0372bc9bdd1bdd7
URAJUV <urajuv1984@gmail.com>
2021-12-08 12:29:05 +0530

    Elliptic Curve Cryptography using elgamal algo

---
+++ Assignment-5-CS6530/ecc.h
@@ -0,0 +1,23 @@
+#include <gmp.h>
+#include <stdlib.h>
+
+
+//Point
+typedef struct point {
+  mpz_t x, y;
+} point;
+
+//Elliptic Curve
+typedef struct elliptic_curve {
+  mpz_t a, b, p;
+  point *base;
+} elliptic_curve;
+
+
+void init_point(point **);
+void destroy_point(point *);
+
+point* ecc_scalar_mul(elliptic_curve *, mpz_t, point *);
+point* ecc_addition(elliptic_curve *, point *, point *);
+point* ecc_doubling(elliptic_curve *, point *);
+
